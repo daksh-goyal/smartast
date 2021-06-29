1787a14c46d194c9369b4d072598bb8e5ea2f52e
Daksh Goyal <dakshgoyal58@gmail.com>
2021-06-29 20:52:24 +0530

    Fixed routing

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
     return (
         <div className='mr4 ml2 mb2 mt2'>
             <Tilt className="Tilt" options={{ max: 100 }} style={{ height: 125, width: 150 }} >
-                <div className="Tilt-inner"> <img className='logo-image' alt='Logo' src={brain} /> </div>
+                <a href="https://daksh-goyal.github.io/smartast/"><div className="Tilt-inner"> <img className='logo-image' alt='Logo' src={brain} /> </div></a>
             </Tilt>
         </div>
     );
