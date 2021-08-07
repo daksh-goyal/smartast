005d1f559b40583bcc72569b3fd37650696745ef
Daksh Goyal <dakshgoyal58@gmail.com>
2021-08-07 22:49:31 +0530

    Minor code fix

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './FaceRecognition.css'
 
 const FaceRecognition = ({ imageUrl, boxes }) => {
-    let boxesDiv = [];
-    boxes.forEach((item, index) => {
-        boxesDiv.push(<div key={index} className='bounding-box' style={{top: item.topRow, right: item.rightCol, bottom: item.bottomRow, left: item.leftCol }}></div>)
-    })
 
     return (
         <div className='center ma'>
             <div className='absolute mt3'>
-                <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto' style={{maxWidth: '500px', width: '90%'}} />
-                <div className='bounding-boxes'>{boxesDiv}</div>
+                <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto' style={{ maxWidth: '500px', width: '90%' }} />
+                {
+                    boxes.map((box, index) =>
+                        <div
+                            key={index}
+                            className='bounding-box'
+                            style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}>
+                        </div>)
+                }
             </div>
         </div>
     );
